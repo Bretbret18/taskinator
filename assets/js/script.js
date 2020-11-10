42092968588c61685ac62da79b32bc524ae58738
Bret Sperry <bret.sperry@gmail.com>
2020-11-09 19:09:57 -0500

    endfourpointfive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-buttonEl.addEventListener("click", createTaskhandler);
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function (event) {
 
+    event.preventDefault();
 
-var createTaskhandler = function () {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
+
+formEl.addEventListener("submit", createTaskHandler);
+
+
+
